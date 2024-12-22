f0dfd490bff68935373009ece4ec914de034897e
Boyko03 <221426@buas.nl>
2024-12-22 19:24:47 +0200

    Create a CustomersView view

--- WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
+++ WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
@@ -20,13 +20,5 @@ namespace WiredBrainCoffee.CustomersApp
         {
             InitializeComponent();
         }
-
-        private void BtnMoveNavigation_OnClick(object sender, RoutedEventArgs e)
-        {
-            var column = Grid.GetColumn(customerListGrid);
-
-            var newColumn = column == 0 ? 2 : 0;
-            Grid.SetColumn(customerListGrid, newColumn);
-        }
     }
 }
---
+++ WiredBrainCoffee.CustomersApp/View/CustomersView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WiredBrainCoffee.CustomersApp.View
+{
+    /// <summary>
+    /// Interaction logic for CustomersView.xaml
+    /// </summary>
+    public partial class CustomersView : UserControl
+    {
+        public CustomersView()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnMoveNavigation_OnClick(object sender, RoutedEventArgs e)
+        {
+            var column = Grid.GetColumn(customerListGrid);
+
+            var newColumn = column == 0 ? 2 : 0;
+            Grid.SetColumn(customerListGrid, newColumn);
+        }
+    }
+}
